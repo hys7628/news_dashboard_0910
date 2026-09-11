26d1fe118498930609a7fbd171398d3f1293a832
hys7628 <hys7628@gmail.com>
2026-09-11 13:58:19 +0900

    mb_12

--- news_dashboard_mb.py
+++ news_dashboard_mb.py
@@ -15,15 +15,19 @@ st.set_page_config(page_title="News Hub", layout="wide", initial_sidebar_state="
 ROOT_DIR = "/Users/han-yunsu/Desktop/AI_Coding_Pyhton/news_dashboard"
 
 # 음원 경로
+# /Users/han-yunsu/Desktop/AI_Coding_Pyhton/news_dashboard
 AUDIO_PATH = os.path.join(ROOT_DIR, "news.mp3")
 
 # 우측 일러스트 에셋 폴더
-ASSET_DIR = ROOT_DIR
+# /Users/han-yunsu/Desktop/AI_Coding_Pyhton/news_dashboard
+ASSET_DIR = os.path.join(ROOT_DIR)
 
 # 좌측 이모티콘 PNG 폴더
+# /Users/han-yunsu/Desktop/AI_Coding_Pyhton/news_dashboard/CATEGORY_EMOJI_PNG
 ICON_DIR = os.path.join(ROOT_DIR, "CATEGORY_EMOJI_PNG")
 
 # 워드 기사 파일 기본 경로 (news_scrapping 또는 신문 스크랩 자동 매칭)
+# /Users/han-yunsu/Desktop/AI_Coding_Pyhton/news_dashboard/news_scrapping
 BASE_DIR = os.path.join(ROOT_DIR, "news_scrapping", "2026")
 if not os.path.exists(BASE_DIR):
     candidate_kr = os.path.join(ROOT_DIR, "news_scrapping", "2026")
